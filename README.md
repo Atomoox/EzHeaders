3b3e273f75fd12e88f994067d71d83c8ed02b841
Atomox_ <aoxresell@gmail.com>
2021-07-24 00:52:09 +0200

    initial

---
+++ README.md
@@ -0,0 +1,21 @@
+# EzHeaders
+
+Makes your life easier by formatting raw chrome headers to Javascript object entries.
+
+## Installation
+
+Idk lol
+
+## Usage
+
+- Select the raw chrome headers
+- Right click
+- Format headers
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/extension.ts
@@ -0,0 +1,31 @@
+import * as vscode from 'vscode';
+
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('headers.format', () => {
+		const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+            editor.edit(editBuilder => {
+                editor.selections.forEach(sel => {
+					try{
+						const range = sel.isEmpty ? document.getWordRangeAtPosition(sel.start) || sel : sel;
+						const word = document.getText(range);
+						const headers: string[] = [];
+						word.split('\n').forEach(line => {
+							const key = line.split(':')[0].trim();
+							const value = line.split(`${key}:`)[1].trim();
+							headers.push(`'${key}': '${value}'`);
+						});
+						editBuilder.replace(range, headers.join(',\n'));
+					}catch (ex) {
+						vscode.window.showErrorMessage(`Couldn't format headers (${ex.message})`);
+					}
+                })
+            })
+        }
+	});
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
